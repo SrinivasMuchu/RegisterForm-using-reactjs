4f2c4c12252f6bb69c1b99d98175095b409f3403
SrinivasMuchu <107020456+SrinivasMuchu@users.noreply.github.com>
2022-11-12 18:53:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import RegistrationForm from './register';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Login Form</h2>
+      <RegistrationForm/>
+    </div>
+  );
+}
+
+export default App;
